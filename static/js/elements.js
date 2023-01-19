ed84cfcea3d8006c0eb932d8f6e51d9597fd22c5
Danilo <danilobernardino786@gmail.com>
2023-01-19 19:25:45 -0300

    initialCommit

---
+++ static/js/elements.js
@@ -0,0 +1,21 @@
+function render_header(){
+
+    header_a.innerHTML = `
+        <header class="d-flex flex-wrap justify-content-center mb-4 border-bottom">
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <img src="../../images/logotipo.png" style="margin-x: 30em" height="100px"></img>
+                <span class="fs-4">Docinhos da Giu</span>
+            </a>
+
+            <div style="margin-top: 40px; margin-bottom: 30px;">
+                <ul class="nav nav-pills">
+                    <li class="nav-item"><a href="#" class="nav_1 active_r">Início</a></li>
+                    <li class="nav-item"><a href="#" class="nav_1">Produtos</a></li>
+                    <li class="nav-item"><a href="#" class="nav_1">Contato</a></li>
+                    <li class="nav-item"><a href="#" class="nav_1">Sobre</a></li>
+                </ul>
+            </div>
+        </header>
+    `
+
+}
